50963b8f4c3544f1807f7de2cd63dccbe6e0d878
Amith21051999 <100063259+Amith21051999@users.noreply.github.com>
2024-11-12 13:33:44 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Serve default files and static files
+// Serve default files and static files adding comment
 app.UseDefaultFiles(new DefaultFilesOptions
 {
     DefaultFileNames = new List<string> { "index.html" }
